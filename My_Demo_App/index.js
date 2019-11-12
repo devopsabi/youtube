8b539920dc7f2f2fa5b3816f237952e67cfead49
Abhishek <devopsabi@gmail.com>
2019-11-12 11:36:23 +0000

    My_Demo_App

--- My_Demo_App/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World! <h1>Demo App Version 02<h1>'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
